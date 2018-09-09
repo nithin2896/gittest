44c04a16c0168659a4125a1f4c4022f5032ec594
nithin2896 <challanithinsaireddy@gmail.com>
2018-09-09 22:51:46 +0530

    ipc

---
+++ ipc/pipe.c
@@ -0,0 +1,6 @@
+#include <unistd.h>
+//int pipe(int pipefd[2]);
+#define _GNU_SOURCE             /* See feature_test_macros(7) */
+#include <fcntl.h>              /* Obtain O_* constant definitions */
+#include <unistd.h>
+
